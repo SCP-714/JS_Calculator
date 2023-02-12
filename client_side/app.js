69602f08aea0686595e60c2f9a3a29926bb6be53
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-12 08:43:14 -0600

    added addition button

--- client_side/app.js
+++ client_side/app.js
@@ -12,3 +12,18 @@ button.addEventListener("keypress", function(event) {
       document.getElementById("equals").click();
     }
   });
+
+  const addButton = document.getElementById("add");
+
+addButton.addEventListener("click",function(){ alert("addition"); });
+
+// Execute a function when the user presses a key on the keyboard
+addButton.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("add").click();
+    }
+  });
