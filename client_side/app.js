d648083f36e5fc749adc11094a4b706aff207a24
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-12 08:46:31 -0600

    added subtraction button

--- client_side/app.js
+++ client_side/app.js
@@ -27,3 +27,18 @@ addButton.addEventListener("keypress", function(event) {
       document.getElementById("add").click();
     }
   });
+
+  const subtractButton = document.getElementById("subtract");
+
+  subtractButton.addEventListener("click",function(){ alert("subtraction"); });
+  
+  // Execute a function when the user presses a key on the keyboard
+  subtractButton.addEventListener("keypress", function(event) {
+      // If the user presses the "Enter" key on the keyboard
+      if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("subtract").click();
+      }
+    });
